9b01c8d83a8ba7523485730b6439d636dd5d6199
Oliiveralien <48074765+Oliiveralien@users.noreply.github.com>
2024-04-20 14:45:57 +1000

    Add files via upload

--- datasets/scratch/LLIE/data/read.py
+++ datasets/scratch/LLIE/data/read.py
@@ -1,15 +1,15 @@
 import os
 import codecs  # 读取文件夹中的文件名
 
-folder = '/root/sk/WeatherDiffusion-main/datasets/scratch/ozan/data/raindrop/test/input'
+folder = '/home/shangkai/MDMS/MDMS-main/datasets/scratch/LLIE/data/lowlight/test/input'
 filenames = os.listdir(folder)
 
 # 将文件名写入 txt 文件
-txt_file = r'/root/sk/WeatherDiffusion-main/datasets/scratch/ozan/data/raindrop/test/raindroptesta.txt'
+txt_file = r'/home/shangkai/MDMS/MDMS-main/datasets/scratch/LLIE/data/lowlight/test/lowlighttesta.txt'
 with codecs.open(txt_file, 'w', 'utf-8') as f:
     for filename in filenames:
         filepath = os.path.dirname(__file__)
-        filepath = os.path.join(filepath, '/raindrop/test/input')
-        filename = '/root/sk/WeatherDiffusion-main/datasets/scratch/ozan/data/raindrop/test/input/'+filename
+        filepath = os.path.join(filepath, '/lowlight/test/input')
+        filename = '/home/shangkai/MDMS/MDMS-main/datasets/scratch/LLIE/data/lowlight/test/input/'+filename
         
         f.write(filename + '\n')
